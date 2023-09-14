22b3b012d8f49d8cc7e42124d4d68278f88144b9
Silvia <silviatc1993@gmail.com>
2023-09-14 11:21:07 +0200

    functions II in progress

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 //CONSTANTES PARA EL LISTADO DE GATOS:
 const addButton = document.querySelector('.js-btn-add');
 const listElement = document.querySelector(`.js-list`);
+const html = '';
 
 //CONSTANTES PARA FORMULARIO AÑADIR GATITO:
 const inputDesc = document.querySelector('.js-input-desc');
@@ -13,29 +14,31 @@ const liItem = document.querySelector('.item'); // BOTÓN + CABECERA
 const newFormElement = document.querySelector('.new-form');
 
 //CONSTANTES PARA EL CAMPO DE FILTRAR O BUSCAR:
-const input_search_desc = document.querySelector('.js_in_search_desc');
-const descrSearchText = input_search_desc.value;
+const input_search_desc = document.querySelector('.js_in_search_desc'); //CASILLA DESCRIPCIÓN
+const input_search_race = document.querySelector('.js_in_search_race'); //CASILLA DE RAZA
+const buttonSearch = document.querySelector('.js-button-search'); //BOTÓN BUSCAR
 
 //CONSTANTES DE LOS GATITOS:
+//KITTEN 1:
 const kitten1Url = `https://dev.adalab.es/gato-siames.webp`;
 const kitten1Name = `Anastacio`;
 const kitten1Desc = `
 Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.`;
 const kitten1Race = `Siamés`;
-const kitten1 = renderKitten(kitten1Url, kitten1Name, kitten1Desc, kitten1Race);
 
+//KITTEN2:
 const kitten2Url = `https://dev.adalab.es/sphynx-gato.webp`;
 const kitten2Name = `Fiona`;
 const kitten2Desc = `Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.`;
 const kitten2Race = `Sphynx-cat`;
-const kitten2 = renderKitten(kitten2Url, kitten2Name, kitten2Desc, kitten2Race);
 
+//KITTEN3:
 const kitten3Url = `https://dev.adalab.es/maine-coon-cat.webp`;
 const kitten3Name = `Cielo`;
 const kitten3Desc = `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.`;
 const kitten3Race = `Maine-Coon`;
-const kitten3 = renderKitten(kitten3Url, kitten3Name, kitten3Desc, kitten3Race);
 
+//FUNCIÓN PARA CREAR GATITOS:
 function renderKitten(photo, name, race, desc) {
   const html = `<li class="card">
         <article>
@@ -51,21 +54,35 @@ function renderKitten(photo, name, race, desc) {
       </li>`;
   return html;
 }
+const kitten3 = renderKitten(kitten3Url, kitten3Name, kitten3Desc, kitten3Race);
+const kitten2 = renderKitten(kitten2Url, kitten2Name, kitten2Desc, kitten2Race);
+const kitten1 = renderKitten(kitten1Url, kitten1Name, kitten1Desc, kitten1Race);
+
+listElement.innerHTML = `${kitten1} ${kitten2} ${kitten3}`;
 
 //PARA ACTIVAR CASILLA DE FILTADO/BÚSQUEDA DE GATITOS
-
-let matchingKittens = '';
 const filterKitten = (event) => {
-  if (kitten1Desc.includes(descrSearchText)) {
+  event.preventDefault();
+  const descrSearchText = input_search_desc.value;
+  const raceSearchText = input_search_race.value;
+  listElement.innerHTML = '';
+  if (
+    kitten1Desc.includes(descrSearchText) ||
+    kitten1Race.includes(raceSearchText)
+  ) {
     listElement.innerHTML += kitten1;
   }
-  if (kitten2Desc.includes(descrSearchText)) {
+  if (
+    kitten2Desc.includes(descrSearchText) ||
+    kitten2Race.includes(raceSearchText)
+  ) {
     listElement.innerHTML += kitten2;
   }
-  if (kitten3Desc.includes(descrSearchText)) {
+  if (
+    kitten3Desc.includes(descrSearchText) ||
+    kitten3Race.includes(raceSearchText)
+  ) {
     listElement.innerHTML += kitten3;
-  } else {
-    listElement.innerHTML = matchingKittens;
   }
 };
 
@@ -108,9 +125,9 @@ function addNewKitten(event) {
   }
 }
 
-//EVENTOS ELISTENER DE LOS BOTONES + Y AÑADIR
+//EVENTOS LISTENER DE LOS BOTONES + Y AÑADIR
 liItem.addEventListener('click', handleClickNewCatForm);
 addButton.addEventListener('click', addNewKitten);
 
-const buttonSearch = document.querySelector('.js-button-search');
+//EVENTO LISTENER PARA EL BOTÓN BUSCAR/FILTRAR GATITOS
 buttonSearch.addEventListener('click', filterKitten);
